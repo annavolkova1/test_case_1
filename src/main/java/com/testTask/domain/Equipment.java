0abc1a978f0b358efda222f0fa95d6125d4b413d
Anna Volkova <annyvolkov@gmail.com>
2021-07-21 21:36:41 +0300

    Add lombok annotations into domain classes

--- src/main/java/com/testTask/domain/Equipment.java
+++ src/main/java/com/testTask/domain/Equipment.java
@@ -1,79 +1,13 @@
 package com.testTask.domain;
 
-import java.util.Objects;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Equipment implements MetaData {
-
-  public Equipment(Long id, String name, Long wellId) {
-
-    this.id = id;
-    this.name = name;
-    this.wellId = wellId;
-  }
-
-  @Override
-  public String toString() {
-
-    return "Equipment{" +
-        "id=" + id +
-        ", name='" + name + '\'' +
-        ", wellId=" + wellId +
-        '}';
-  }
-
-  @Override
-  public boolean equals(Object object) {
-
-    if (this == object) {
-      return true;
-    }
-    if (object == null || getClass() != object.getClass()) {
-      return false;
-    }
-    Equipment equipment = (Equipment) object;
-    return Objects.equals(id, equipment.id) && Objects.equals(name, equipment.name)
-        && Objects.equals(wellId, equipment.wellId);
-  }
-
-  @Override
-  public int hashCode() {
-
-    return Objects.hash(id, name, wellId);
-  }
-
-  public Long getWellId() {
-
-    return wellId;
-  }
-
-  public void setWellId(Long wellId) {
-
-    this.wellId = wellId;
-  }
-
-  public void setId(Long id) {
-
-    this.id = id;
-  }
-
-  public void setName(String name) {
-
-    this.name = name;
-  }
-
-  public Equipment() {
-
-  }
-
-  public Long getId() {
-
-    return id;
-  }
-
-  public String getName() {
-
-    return name;
-  }
 
   private Long id;
   private String name;
